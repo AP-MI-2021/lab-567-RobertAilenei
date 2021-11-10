d908d3c730b7da86a5ca71a9d7727aaccec135de
RobertAilenei <robertailenei@yahoo.com>
2021-11-10 12:04:39 +0200

    Am facut ceva modificari la functia de test undo/redo

--- Tests/test_undo_redo.py
+++ Tests/test_undo_redo.py
@@ -18,14 +18,15 @@ def testare():
     assert len(lst_obiecte) == 1
     lst_obiecte, current_version = handle_undo(list_version, current_version)
     assert len(lst_obiecte) == 0
-    assert handle_undo(list_version, current_version) is None
+    lst_obiecte, current_version = handle_undo(list_version, current_version)
+    assert len(lst_obiecte) == 0
     lst_obiecte = create(lst_obiecte, 1, 'masa', 'desc', 150, 'erfs')
     list_version, current_version = handle_new_list(list_version, current_version, lst_obiecte)
     lst_obiecte = create(lst_obiecte, 2, 'dulap', 'desc', 70, 'arfs')
     list_version, current_version = handle_new_list(list_version, current_version, lst_obiecte)
     lst_obiecte = create(lst_obiecte, 3, 'sac', 'desc', 80, 'arss')
     list_version, current_version = handle_new_list(list_version, current_version, lst_obiecte)
-    assert handle_redo(list_version, current_version) is None
+    lst_obiecte, current_version = handle_redo(list_version, current_version)
     assert len(lst_obiecte) == 3
     lst_obiecte, current_version = handle_undo(list_version, current_version)
     lst_obiecte, current_version = handle_undo(list_version, current_version)
@@ -38,7 +39,8 @@ def testare():
     lst_obiecte, current_version = handle_undo(list_version, current_version)
     assert len(lst_obiecte) == 1
     lst_obiecte = create(lst_obiecte, 4, 'masa', 'desc', 150, 'erfs')
-    assert handle_redo(list_version, current_version) is None
+    list_version, current_version = handle_new_list(list_version, current_version, lst_obiecte)
+    lst_obiecte, current_version = handle_redo(list_version, current_version)
     assert len(lst_obiecte) == 2
     lst_obiecte, current_version = handle_undo(list_version, current_version)
     assert len(lst_obiecte) == 1
@@ -47,4 +49,8 @@ def testare():
     lst_obiecte, current_version = handle_redo(list_version, current_version)
     lst_obiecte, current_version = handle_redo(list_version, current_version)
     assert len(lst_obiecte) == 2
-    assert handle_redo(list_version, current_version) is None
+    lst_obiecte, current_version = handle_redo(list_version, current_version)
+    assert len(lst_obiecte) == 2
+
+
+testare()
